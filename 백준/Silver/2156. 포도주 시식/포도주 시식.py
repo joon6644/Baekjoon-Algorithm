bc5de4d2e49e87ea05a7c7b4c203188e5fbd1df7
joon6644 <joon6644@gmail.com>
2026-01-24 20:36:08 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+INF = float('inf')
+
+n = int(input())
+
+array = [0]
+for _ in range(n):
+    array.append(int(input()))
+if n < 2:
+    print(sum(array))
+    sys.exit()
+
+dp = [0] * (n + 1)
+dp[1] = array[1]
+dp[2] = array[1] + array[2]
+
+for i in range(3, n + 1):
+    dp[i] = max(dp[i - 1], dp[i - 2] + array[i], dp[i - 3] + array[i - 1] + array[i])
+
+print(dp[-1])
