8f0de1666ad96a91908fb49d5795a8ccb8e3703f
joon6644 <joon6644@gmail.com>
2026-01-25 22:11:25 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 248 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
@@ -8,27 +8,16 @@ if n == 0:
     print(0)
     print(0)
     sys.exit()
+elif n < 0 and n % 2 == 0:
+    print(-1)
+else:
+    print(1)
 
+n = abs(n)
 pre = 0
 cur = 1
 
-if n >= 0:
-    for i in range(2, n + 1):
-        nxt = (cur + pre) % mod
-        pre = cur
-        cur = nxt
- 
-elif n < 0:
-    n = - n
-    for i in range(2, n + 1):
-        nxt = pre - cur
-        if nxt > 0:
-            nxt %= mod
-        else:
-            nxt = - ((- nxt) % mod)
-        pre = cur
-        cur = nxt
+for i in range(2, n + 1):
+    pre, cur = cur, (cur + pre) % mod
 
-ans = cur
-print(1 if ans > 0 else -1)
-print(abs(ans) % mod)
+print(cur)
