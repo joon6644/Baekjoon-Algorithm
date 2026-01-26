93406854fc4df64229753158e3f90e7f9d234961
joon6644 <joon6644@gmail.com>
2026-01-26 23:07:12 +0900

    [Silver III] Title: 병든 나이트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+if N == 1:
+    print(1)
+
+elif N == 2:
+    if M >= 7:
+        print(4)
+    else:
+        print((M + 1) // 2)
+
+else:
+    if M < 7:
+        print(min(M, 4))
+    else:
+        print(M - 2)
