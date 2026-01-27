92f393e13d5ecd5f0565fc0bf970de913d7bb00a
joon6644 <joon6644@gmail.com>
2026-01-28 00:06:14 +0900

    [Gold V] Title: 주사위, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1041. 주사위/주사위.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dice = list(map(int, input().split()))
+
+if N == 1:
+    print(sum(sorted(dice)[:5]))
+    sys.exit()
+elif N == 2:
+    f = 0
+    e = 4
+    v = 4
+else:
+    f = 5 * N**2 - 16 * N + 12
+    e = 8 * N - 12  
+    v = 4
+
+nums = []
+for i in range(3):
+    nums.append(min(dice[i], dice[5 - i]))
+nums.sort()
+for i in range(2):
+    nums[i + 1] += nums[i]
+
+print(f * nums[0] + e * nums[1] + v * nums[2])
