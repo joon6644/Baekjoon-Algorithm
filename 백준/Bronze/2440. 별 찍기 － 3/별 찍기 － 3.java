d652fa6291b68a182209ebee85be5d91656bcd80
joon6644 <joon6644@gmail.com>
2026-01-31 18:33:51 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 248 ms, Memory: 18120 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = N; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
