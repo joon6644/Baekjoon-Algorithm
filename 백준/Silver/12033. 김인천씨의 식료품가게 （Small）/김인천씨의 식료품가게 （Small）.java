96f0682be01730bf72879459eb1e0726351c3b21
joon6644 <joon6644@gmail.com>
2026-02-12 12:19:54 +0900

    [Silver V] Title: 김인천씨의 식료품가게 (Small), Time: 256 ms, Memory: 19612 KB -BaekjoonHub

---
+++ 백준/Silver/12033. 김인천씨의 식료품가게 （Small）/김인천씨의 식료품가게 （Small）.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            long[] arr = new long[2 * N];
+
+            for (int j = 0; j < 2 * N; j++) {
+                arr[j] = sc.nextInt();
+            }
+
+            Arrays.sort(arr);
+            Queue<Long> q = new LinkedList<>();
+
+            System.out.print("Case #" + (i + 1) + ": ");
+
+            for (long item : arr) {
+                if (!q.isEmpty() && q.peek() == item) {
+                    q.poll();
+                } else {
+                    System.out.print(item + " ");
+                    q.offer(item * 4 / 3);
+                }
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
