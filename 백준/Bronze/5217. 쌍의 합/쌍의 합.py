ba5f6ad097bfb47b33ef3399dfadac06d47659fb
joon6644 <joon6644@gmail.com>
2026-02-22 13:39:38 +0900

    [Bronze III] Title: 쌍의 합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5217. 쌍의 합/쌍의 합.py
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.py
@@ -6,8 +6,6 @@ T = int(input())
 for _ in range(T):
     n = int(input())
 
-    t = []
-    for i in range(1, (n + 1) // 2):
-        t.append(f"{i} {n - i}")
+    t = [f"{i} {n - i}" for i in range(1, (n + 1) // 2)]
 
     sys.stdout.write(f"Pairs for {n}: {', '.join(t)}\n")
